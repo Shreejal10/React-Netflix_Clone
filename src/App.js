9f9360e052178a1faed107d7ea0a36762212791f
Shreejal Dhungana <dshreejal@gmail.com>
2022-09-05 22:13:56 +0545

    firebase setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
