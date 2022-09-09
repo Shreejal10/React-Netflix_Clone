f931fc3b5c6b6454313f31418a2673a7e08deae8
Shreejal Dhungana <dshreejal@gmail.com>
2022-09-09 22:04:56 +0545

    firebase login signup authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const { user, logIn } = UserAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError('')
+        try {
+            await logIn(email, password)
+            navigate('/')
+        }
+        catch (error) {
+            console.log(error);
+            setError(error.message);
+        }
+    }
+
     return (
         <div className='w-full h-screen'>
             <img src="https://assets.nflxext.com/ffe/siteui/vlv3/701eec50-4b87-4dc0-9d00-b0f54025dc36/1900541a-9bb5-477d-bfd2-a1a370bc9493/NP-en-20220905-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="/" className=' absolute w-full h-full object-cover' />
@@ -10,9 +30,10 @@ const Login = () => {
                 <div className='max-w-[450px] h-[500px] mx-auto bg-black/75 text-white mt-11'>
                     <div className='max-w-[320px] mx-auto py-16'>
                         <h1 className='text-3xl font-bold'>Sign In</h1>
-                        <form className='w-full flex flex-col py-4'>
-                            <input className='p-3 my-2 bg-gray-600 rounded' type="email" placeholder='Email' />
-                            <input className='p-3 my-2 bg-gray-600 rounded' type="password" placeholder='Password' />
+                        {error ? <p className='p-3 bg-red-400 my-3'>{error}</p> : null}
+                        <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
+                            <input onChange={(e) => setEmail(e.target.value)} className='p-3 my-2 bg-gray-600 rounded' type="email" placeholder='Email' />
+                            <input onChange={(e) => setPassword(e.target.value)} className='p-3 my-2 bg-gray-600 rounded' type="password" placeholder='Password' />
                             <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign In</button>
                             <div className='flex justify-between items-center text-sm text-gray-600'>
                                 <p><input type="checkbox" className='mr-2' />Remember me</p>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Signup = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const { user, signUp } = UserAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await signUp(email, password)
+            navigate('/')
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className='w-full h-screen'>
@@ -11,9 +29,9 @@ const Signup = () => {
                     <div className='max-w-[450px] h-[500px] mx-auto bg-black/75 text-white mt-11'>
                         <div className='max-w-[320px] mx-auto py-16'>
                             <h1 className='text-3xl font-bold'>Sign Up</h1>
-                            <form className='w-full flex flex-col py-4'>
-                                <input className='p-3 my-2 bg-gray-600 rounded' type="email" placeholder='Email' />
-                                <input className='p-3 my-2 bg-gray-600 rounded' type="password" placeholder='Password' />
+                            <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
+                                <input onChange={(e) => setEmail(e.target.value)} className='p-3 my-2 bg-gray-600 rounded' type="email" placeholder='Email' />
+                                <input onChange={(e) => setPassword(e.target.value)} className='p-3 my-2 bg-gray-600 rounded' type="password" placeholder='Password' />
                                 <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
                                 <div className='flex justify-between items-center text-sm text-gray-600'>
                                     <p><input type="checkbox" className='mr-2' />Remember me</p>
